ba9aa401a1c02b76a24657f48af3f2d44c76b211
Evin Dunn <evindunn14@gmail.com>
2020-08-24 23:59:53 -0700

    webpack changes

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,31 @@
 const path = require("path");
 const MiniCss = require("mini-css-extract-plugin");
 const OptimizeCss = require("optimize-css-assets-webpack-plugin");
+const OnBuild = require("on-build-webpack");
+const fs = require("fs");
 
 module.exports = {
-    entry: "./src/main.less",
     mode: "production",
+    entry: { "css-utils": path.resolve(__dirname, "src", "main.less") },
+    output: { path: path.join(__dirname, "dist") },
+    optimization: { minimizer: [new OptimizeCss()] },
+    plugins: [
+        new MiniCss({ filename: "[name].css" }),
+        new OnBuild(() => fs.unlinkSync(path.join(__dirname, "dist", "css-utils.js")))
+    ],
     stats: {
         all: false,
         assets: true,
         errors: true
-    },
-    output: {
-        path: path.join(__dirname, "dist")
-    },
-    plugins: [new MiniCss()],
-    optimization: {
-        minimizer: [new OptimizeCss({})]
     },
     module: {
         rules: [
             {
                 test: /\.(c|le)ss$/i,
                 use: [
-                    { loader: MiniCss.loader },
-                    { loader: "css-loader" },
-                    { loader: "less-loader" }
+                    MiniCss.loader,
+                    "css-loader",
+                    "less-loader"
                 ],
             },
         ],
